1d470be073e9dcf43b6a80170e177a84b82d78be
Adam Hartz <ahartz1@gmail.com>
2015-10-26 17:35:47 -0400

    First pass definitions for Activity, Datapoint

--- jal_stats/stats/models.py
+++ jal_stats/stats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Activity(models.Model):
+    full_description = models.CharField(max_length=255)
+    units = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.full_description
+
+
+class Datapoint(models.Model):
+    activity = models.ForeignKey('Activity')
+    reps = models.PositiveIntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} {} at {}'.format(self.reps,
+                                    self.activity.units,
+                                    self.timestamp)
