37fe8926614eded622226e967dc0334381b95af4
Adam Hartz <ahartz1@gmail.com>
2015-10-26 19:23:44 -0400

    Add app and PostgreSQL to settings.py

--- jal_stats/jal_stats/settings.py
+++ jal_stats/jal_stats/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')qw)*wnq3qn4zb!92bdpq#$u^wuqic$%buhpupeaf2$cd!v!o0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stats',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,13 +75,24 @@ WSGI_APPLICATION = 'jal_stats.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jal_stats',
+        'USER': 'jal_stats',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
