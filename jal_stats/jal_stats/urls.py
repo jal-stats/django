8fc9c36407f43fe32717e18bb4ba5ef6bb54d07a
Adam Hartz <ahartz1@gmail.com>
2015-10-26 19:44:08 -0400

    Add rest_framework routers and paths to URLs, settings.py

--- jal_stats/jal_stats/urls.py
+++ jal_stats/jal_stats/urls.py
@@ -15,7 +15,21 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+from stats import views as stats_views
+
+
+router = routers.DefaultRouter()
+router.register(r'activities', stats_views.ActivityViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+
+    url(r'^', include(router.urls)),
+
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
 ]
