f4d665a6f768962701e771e9346e86e0afafcb61
Adam Hartz <ahartz1@gmail.com>
2015-10-26 21:21:13 -0400

    Prep for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jal_stats.wsgi --log-file -
--- jal_stats/jal_stats/settings.py
+++ jal_stats/jal_stats/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
