3838e44a397fdb4b605ead875b7c6ebc5787644d
Adam Hartz <ahartz1@gmail.com>
2015-10-26 20:50:31 -0400

    Add 'id' to both Serializers

--- jal_stats/stats/serializers.py
+++ jal_stats/stats/serializers.py
@@ -5,10 +5,10 @@ from .models import Activity, Datapoint
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Activity
-        fields = ('user', 'full_description', 'units', 'url')
+        fields = ('id', 'user', 'full_description', 'units', 'url')
 
 
 class DatapointSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Datapoint
-        fields = ('user', 'activity', 'reps', 'timestamp', 'url')
+        fields = ('id', 'user', 'activity', 'reps', 'timestamp', 'url')
