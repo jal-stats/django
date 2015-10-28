794d059501480091174df836e21fe8d956a21e86
Adam Hartz <ahartz1@gmail.com>
2015-10-28 14:56:23 -0400

    Initial work on stat serializer for post

--- jal_stats/jal_stats/settings.py
+++ jal_stats/jal_stats/settings.py
@@ -128,7 +128,14 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_AllOW_ALL = True
-
+CORS_ALLOW_HEADERS = (
+    'accept-encoding',
+    'x-requested-with',
+    'accept',
+    'origin',
+    'authorization',
+    'x-csrftoken',
+    )
 # if DEBUG is True:
 #     CORS_ORIGIN_ALLOW_ALL = True
 # else:
